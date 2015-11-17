2a96d75902111a99a2819d447a29b59937a57924
ANANTHAKRISHNAN U S <krishnan793@gmail.com>
2015-11-18 01:12:35 +0530

    Added Files. Need much more work to port completly to ardrone_control modules

---
+++ src/autonomous_landingNeural.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# This node will perform autonomous landing using neural network.
+# https://github.com/krishnan793/ar2landing_neural
+# Author : Ananthakrishnan U S
+
+import roslib
+roslib.load_manifest("ardrone_control")
+
+import rospkg
+import rospy
+import time
+import message_filters
+import numpy as np
+from std_msgs.msg import Empty
+from sensor_msgs.msg import Range
+from geometry_msgs.msg import Twist
+from visualization_msgs.msg import Marker
+from drone_controller import BasicDroneController
+
+def land():
+	controller.SendLand()
+	rospy.loginfo(rospy.get_caller_id() + " Quad copter Landed")
+	time.sleep(1)
+	return
+
+def sigmoid(num):
+	return 1.0/(1+np.exp(-1*num))
+
+def normalize(data,MAX):
+	return (data+1)/MAX
+	
+def callback(data):
+	if(controller.commandTimer==None):
+		controller.StartSendCommand()
+	x1 = normalize(data.pose.position.x,MAX)
+	x2 = normalize(data.pose.position.y,MAX)
+	x3 = normalize(data.pose.position.z,MAX)
+	x4 = normalize(data.pose.orientation.x,MAX)
+	x5 = normalize(data.pose.orientation.y,MAX)
+	
+	if(x3<=0.55):
+		land()
+		rospy.signal_shutdown("Landed")
+		
+	X=[1,x1,x2,x3,x4,x5]
+	#print X
+    #print w1
+    #print w2
+	Y=sigmoid(np.dot(X,w1))
+    #print Y
+	Z=sigmoid(np.dot(Y,w2))
+	Z=np.around(Z, decimals=2)
+	rospy.loginfo(rospy.get_caller_id() + " Input  %s %s %s %s %s", format(x1, '.2f'),format(x2, '.2f'),format(x3, '.2f'),format(x4, '.2f'),format(x5, '.2f'))
+	#print Z
+	control(Z)
+    #sonar0 = float(data.range)
+    #print "ads",data1
+def load_weights(file):
+	fp=np.load(file)
+	w1=fp['arr_0']
+	w2=fp['arr_1']
+	#print w1
+	#print w2
+	return [w1,w2]
+	
+def control(Z):
+	cntrl = Twist()
+	x=2*Z[0]-1
+	y=2*Z[1]-1
+	z=2*Z[2]-1
+	angz=2*Z[3]-1
+	rospy.loginfo(rospy.get_caller_id() + " Output %s %s %s %s", x,y,z,angz)
+	controller.SetCommand(y,x,angz,z)
+
+def limit_reading(data,MAX):
+	if(data>MAX):
+		data=MAX
+	return data
+def listener():
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+	
+	
+
+	rospy.Subscriber("/visualization_marker", Marker, callback)
+	rospy.spin()
+
+if __name__ == '__main__':
+	# get an instance of RosPack with the default search paths
+	rospack = rospkg.RosPack()
+	#pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+	# get the file path for rospy_tutorials
+	path = rospack.get_path('ar2landing_gazebo')
+	rospy.init_node('NNcontroller', anonymous=True)
+	MAX = 2
+	[w1,w2]=load_weights(path+"/data/weights.npz")
+	controller = BasicDroneController()
+	listener()
+
---
+++ src/autonomous_search.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# This node will perform a lawn mower search pattern unti a tag is detected
+# https://github.com/krishnan793/ar2landing_neural
+# Author : Ananthakrishnan U S
+
+import roslib
+# ardrone_tutorials has a good ardrone classs for controlling drone. load_manifest import that python module
+roslib.load_manifest("ardrone_control")
+
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty
+from visualization_msgs.msg import Marker
+from drone_controller import BasicDroneController
+import time
+
+
+
+def detect_tag(data):
+	# This will set a flag and stop executing autonomous search
+	controller.SetCommand(0,0,0,0)
+	controller.StopSendCommand()
+	global detect
+	detect = 1
+
+def sleep(sec):
+	while(sec and not rospy.is_shutdown()):
+		if(detect==1):
+			break
+		time.sleep(1)
+		sec -= 1
+
+def reset():
+	nothing = Empty()
+	print nothing
+	pub = rospy.Publisher('/ardrone/reset', Empty, queue_size=10)
+	time.sleep(1)
+	pub.publish(nothing)
+	return
+
+			
+def takeoff():
+	nothing = Empty()
+	print nothing
+	pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=10)
+	time.sleep(1)
+	pub.publish(nothing)
+	return
+
+def land():
+	nothing = Empty()
+	print "Landing"
+	pub = rospy.Publisher('/ardrone/land', Empty, queue_size=10)
+	time.sleep(1)
+	pub.publish(nothing)
+	time.sleep(1)
+	return
+
+def reset_drone(pub1):
+	control = Twist()
+	control.linear.x=0
+	control.linear.y=0
+	control.linear.z=0
+	pub1.publish(control)
+	time.sleep(1)
+def search_pattern():
+	#control = Twist()
+	#(linear.y,linear.x,angular.z,linear.z)
+	controller.SetCommand(0,0,0,1.5)
+	#control.linear.x=0
+	#control.linear.y=0
+	#control.linear.z=1.5
+	#pub1.publish(control)
+	sleep(5)
+	controller.SetCommand(0,0,0,0)
+	sleep(1)
+	#control.linear.z=0
+	#pub1.publish(control)
+	while not rospy.is_shutdown() and detect == 0:
+		controller.SetCommand(0,1,0,0)
+		#control.linear.x=1
+		#control.linear.y=0
+		#pub1.publish(control)
+		sleep(1)
+		controller.SetCommand(-1,0,0,0)
+		#control.linear.x=0
+		#control.linear.y=-1
+		#pub1.publish(control)
+		sleep(9)
+		controller.SetCommand(0,1,0,0)
+		#control.linear.x=1
+		#control.linear.y=0
+		#pub1.publish(control)
+		sleep(1)
+		controller.SetCommand(1,0,0,0)
+		#control.linear.x=0
+		#control.linear.y=1
+		#pub1.publish(control)
+		sleep(9)	
+if __name__ == '__main__':
+	rospy.init_node('autonomous_search', anonymous=True)
+	pub1 = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+	controller = BasicDroneController()
+	controller.StartSendCommand()
+	time.sleep(1)
+	detect = 0
+	controller.SendTakeoff()
+	controller.StartSendCommand()
+	pub2 = rospy.Subscriber("/visualization_marker", Marker, detect_tag)
+	search_pattern()
+	reset_drone(pub1)
+	rospy.spin()
+
+
